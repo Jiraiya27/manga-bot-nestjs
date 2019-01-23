bc17f7e1e6417b64a4340e89fcb8f2fa933d5470
Jiraiya27 <jiraiya279@gmail.com>
2019-01-23 15:10:07 +0700

    Add rss parser service

---
+++ src/rss/rss.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { RssService } from './rss.service';
+
+describe('RssService', () => {
+  let rssService: RssService;
+
+  beforeAll(async () => {
+    const app = await Test.createTestingModule({
+      providers: [RssService],
+    }).compile();
+    rssService = app.get(RssService);
+  });
+
+  describe('parseUrl', () => {
+    it('Should fail for non-valid urls', async () => {
+      const url = 'something not a url';
+      let e;
+      try {
+        await rssService.parseUrl(url);
+      } catch (error) {
+        e = error;
+      }
+      expect(e.message).toMatch(/is not a valid url/);
+    });
+
+    it('Should fail for non-rss urls', async () => {
+      const url = 'https://google.com';
+      let e;
+      try {
+        await rssService.parseUrl(url);
+      } catch (error) {
+        e = error;
+      }
+      expect(e.message).toMatch(/Could not parse/);
+    });
+  });
+});
---
+++ src/rss/rss.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import Parser, { Output } from 'rss-parser';
+import normalizeUrl from 'normalize-url';
+
+@Injectable()
+export class RssService {
+  private parser = new Parser();
+
+  async parseUrl(url: string) {
+    let normalized;
+    try {
+      normalized = normalizeUrl(url);
+    } catch (error) {
+      throw new Error(`${url} is not a valid url`);
+    }
+
+    let rssFeed: Output;
+    try {
+      rssFeed = await this.parser.parseURL(normalized);
+    } catch (error) {
+      throw new Error(`Could not parse ${url} as a rss feed.`);
+    }
+    return rssFeed;
+  }
+}
